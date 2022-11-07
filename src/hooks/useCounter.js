91b6441166208d1203294fb44b05bbf2def19708
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-11-07 19:05:00 +0100

    docs(useCounter): add docs

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -2,7 +2,16 @@ import { useState } from "react"
 
 export const useCounter = (initialValue = 10) => {
     const [counter, setCounter] = useState(initialValue);
+
+    /* It takes a value as an argument, and adds the value to the counter. */
     const increment = (value = 1) => setCounter(counter + value);
+
+    /**
+     * It takes a value as an argument, and if the counter is greater than 0, it subtracts the value
+     * from the counter
+     * @param [value=1] - The value to decrement the counter by.
+     * @returns the value of the counter.
+     */
     const decrement = (value = 1) => {
         if (counter === 0) return;
         setCounter(counter - value);
