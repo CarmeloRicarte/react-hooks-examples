08af7e5903e0250ceb0da9f015319e635cc0779c
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-11-09 10:28:13 +0100

    feat: useMemo hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Memorize } from "./memos/Memorize";
+import { MemoHook } from "./memos/MemoHook";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Memorize />
+    <MemoHook />
   </React.StrictMode>
 );
