6f3dda6df8342b551accbe112f1ee2b2e8f1292c
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-11-15 09:27:50 +0100

    feat(useContext): add components, react-router-dom and routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { TodoApp } from "./useReducer/TodoApp";
+import { BrowserRouter } from "react-router-dom";
+
 import "./index.css";
+import { MainApp } from "./useContext/MainApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <TodoApp />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <MainApp />
+    </React.StrictMode>
+  </BrowserRouter>
 );
