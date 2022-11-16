973094652fb8ee9a069627087ead392a0aa2c985
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-11-16 19:26:40 +0100

    refactor(useCounter): use the current value when setting counter

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,28 +1,27 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export const useCounter = (initialValue = 10) => {
-    const [counter, setCounter] = useState(initialValue);
+  const [counter, setCounter] = useState(initialValue);
 
-    /* It takes a value as an argument, and adds the value to the counter. */
-    const increment = (value = 1) => setCounter(counter + value);
+  /* It takes a value as an argument, and adds the value to the counter. */
+  const increment = (value = 1) => setCounter((current) => current + value);
 
-    /**
-     * It takes a value as an argument, and if the counter is greater than 0, it subtracts the value
-     * from the counter
-     * @param [value=1] - The value to decrement the counter by.
-     * @returns the value of the counter.
-     */
-    const decrement = (value = 1) => {
-        if (counter === 0) return;
-        setCounter(counter - value);
-    }
-    const resetCounter = () => setCounter(initialValue);
+  /**
+   * It takes a value as an argument, and if the counter is greater than 0, it subtracts the value
+   * from the counter
+   * @param [value=1] - The value to decrement the counter by.
+   * @returns the value of the counter.
+   */
+  const decrement = (value = 1) => {
+    if (counter === 0) return;
+    setCounter((current) => current - value);
+  };
+  const resetCounter = () => setCounter(initialValue);
 
-    return {
-        counter,
-        increment,
-        decrement,
-        resetCounter
-    }
-
-}
+  return {
+    counter,
+    increment,
+    decrement,
+    resetCounter,
+  };
+};
