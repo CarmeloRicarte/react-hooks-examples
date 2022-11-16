18ab26410e5f5161f170d536151ab60395075608
Carmelo Ricarte Rocamora <cricarterocamora@icloud.com>
2022-11-16 19:58:50 +0100

    style(useForm): format document

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,30 +1,30 @@
 import { useState } from "react";
 
 export const useForm = (initialForm = {}) => {
-    const [formState, setFormState] = useState(initialForm);
+  const [formState, setFormState] = useState(initialForm);
 
-    /**
-     * When the input changes, update the state of the form with the new value of the input.
-     */
-    const onInputChange = ({ target }) => {
-        const { name, value } = target;
-        setFormState({
-            ...formState, // maintain state of other form fields
-            [name]: value, // update state of field that is changed
-        });
-    };
+  /**
+   * When the input changes, update the state of the form with the new value of the input.
+   */
+  const onInputChange = ({ target }) => {
+    const { name, value } = target;
+    setFormState({
+      ...formState, // maintain state of other form fields
+      [name]: value, // update state of field that is changed
+    });
+  };
 
-    /**
-     * It resets the form to its initial state.
-     */
-    const onResetForm = () => {
-        setFormState(initialForm);
-    }
+  /**
+   * It resets the form to its initial state.
+   */
+  const onResetForm = () => {
+    setFormState(initialForm);
+  };
 
-    return {
-        ...formState, // for return every form control of state
-        formState,
-        onInputChange,
-        onResetForm
-    }
-}
+  return {
+    ...formState, // for return every form control of state
+    formState,
+    onInputChange,
+    onResetForm,
+  };
+};
